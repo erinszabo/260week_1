b9b734e986d7eb1fcfa4ed8b53044e778361c7ba
erinszabo <55733370+erinszabo@users.noreply.github.com>
2020-10-04 15:07:48 -0700

    separated fraction, started unit testing

---
+++ Fraction.py
@@ -0,0 +1,84 @@
+"""
+To Do:
+-fix all doc strings
+-create unit tests for each function
+-fix gcd function
+-more...
+"""
+class Fraction:
+    # __den: int
+
+    """ Creates a fraction object """
+
+    def __init__(self, top: int = 0, bottom: int = 1):
+        self.__num: int = top
+        self.__den: int = bottom
+
+    """ replacing default String  """
+
+    def __str__(self):
+        return f"{self.__num}/{self.__den}"
+
+    """ This is essentially a getter """
+
+    @property
+    def num(self):
+        return self.__num
+
+    @property
+    def den(self):
+        return self.__den
+
+    def __add__(self, otherfraction):
+        """"
+        adds the fractions, not simplified
+        :type otherfraction: Fraction
+        """
+        newnum = self.num * otherfraction.den + self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+        return Fraction(newnum, newden)
+
+    def gcd(m, n):
+        """
+        finds the greatest common denominator.
+        :type n: int
+        :type m: int
+        """
+        while m % n != 0:  # while m doesn't divide evenly into n // check if m % n (modulo of the 2 given) != 0
+            oldm = m       # swap m and n
+            oldn = n
+            m = oldn
+            n = oldm % oldn  # set n equal to the modulo of oldm and oldn
+        return n
+
+    def __eq__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+        return firstnum == secondnum
+
+''' 
+TO FIX:
+- figure out gcd function 
+- create unit tests for each function 
+
+UNIT TESTING FOR FRACTION CLASS BELOW
+'''
+
+myfraction = Fraction(3, 5)
+print(myfraction)
+otherfraction = Fraction(2, 9)
+print(otherfraction)
+frac_sum = myfraction.__add__(otherfraction)
+print(f" __add__ result : {frac_sum} ")
+oden = otherfraction.den
+mden = myfraction.den
+print(f"oden = {oden} , mden = {mden}")
+
+
+
+
+# x = gcd(20, 10)
+# print(x)
+
+print()
+print()
---
+++ test_frac.py
@@ -0,0 +1,9 @@
+import unittest
+import chapter_1
+import Fraction
+
+
+class Test_Frac(unittest.TestCase):
+
+    def test_add(self):
+        a = Fraction(3, 5)
